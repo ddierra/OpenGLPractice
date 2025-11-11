10e102d195fe11bc6ecdad81e7ceda8f43c5dbac
Deedz <dierra.delp@gmail.com>
2025-11-11 12:11:15 -0500

    Creatred square

--- src/main.cpp
+++ src/main.cpp
@@ -89,14 +89,22 @@ int main() {
 
     //set up vertex data (and buffer(s)) and configure vertex attributes
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, //bottom left
-         0.5f, -0.5f, 0.0f, //bottom right
-         0.0f,  0.5f, 0.0f  //top
+        0.5f, 0.5f, 0.0f,  //top right
+        0.5f, -0.5f, 0.0f, //bottom right
+       -0.5f, -0.5f, 0.0f, //bottom left
+         -0.5f, 0.5f, 0.0f   //top left
     };
 
-    unsigned int VBO, VAO;
+    unsigned int indices[] = {  //note that we start from 0!
+        0, 1, 3,   //first triangle
+        1, 2, 3    //second triangle
+    };
+    
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
     //bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
 
@@ -105,6 +113,10 @@ int main() {
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     //note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -127,7 +139,8 @@ int main() {
         //draw our first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         //glfw swap buffers and poll IO events
         glfwSwapBuffers(window);
@@ -136,6 +149,7 @@ int main() {
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     //terminate GLFW
